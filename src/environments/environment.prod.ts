5ee4a463b25e39141731998c561c406f8ced87d8
De La Cruz <c.o.dela.cruz@accenture.com>
2022-10-05 20:54:35 +0800

    Add API to production

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,8 @@
 export const environment = {
-  production: true
+  production: true,
+  weatherApiBaseUrl: 'https://weatherapi-com.p.rapidapi.com/current.json',
+  xRapidApiHostHeaderName: 'X-RapidAPI-Host',
+  xRapidApiHostHeaderValue: 'weatherapi-com.p.rapidapi.com',
+  xRapidApiKeyHeaderName: 'X-RapidAPI-Key',
+  xRapidApiKeyHeaderValue: '5f41965861msh28bf865305a8975p1acf92jsn05aa1eba7ed9'
 };
